15c82e3466c17093f638df28ce9311605845e27c
voin_sr <sergey.voinov@dlr.de>
2017-12-04 10:56:20 +0100

    added map and its dependencies

--- src/app/ukis.module.ts
+++ src/app/ukis.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from 'clarity-angular';
 
+import {MapstateServiceModule} from '@ukis/services/src/app/mapstate/mapstate.module';
+import { OlMapModule } from '@ukis/ol-map//src/app/ol-map/ol-map.module'
+
 
 import { UkisComponent } from './ukis.component';
 import { GlobalAlertComponent } from './global-alert/global-alert.component';
@@ -18,7 +21,9 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ClarityModule.forRoot()
+    ClarityModule.forRoot(),
+    MapstateServiceModule.forRoot(),
+    OlMapModule
   ],
   providers: [],
   bootstrap: [UkisComponent]
