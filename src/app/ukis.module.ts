5830d0b509717ad65177a627242fbda1dd78dbee
voin_sr <sergey.voinov@dlr.de>
2017-12-05 14:30:27 +0100

    banana

--- src/app/ukis.module.ts
+++ src/app/ukis.module.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
 import {MapstateServiceModule} from '@ukis/services/src/app/mapstate/mapstate.module';
 import { LayersServiceModule } from '@ukis/services/src/app/layers/layers.module';
 import { MapcontrolModule } from '@ukis/mapcontrol/src/app/mapcontrol/mapcontrol.module';
-//import {  } from '@ukis/mapcontrol/src/app/mapnavigator/mapnavigator.module';
+import { MapnavigatorModule } from '@ukis/mapcontrol/src/app/mapnavigator/mapnavigator.module';
 import { OlMapModule } from '@ukis/ol-map//src/app/ol-map/ol-map.module';
 import { AppStoreService } from './shared/app-store.service';
 
@@ -32,7 +32,8 @@ import { LayerlistComponent } from './layerlist/layerlist.component';
     MapstateServiceModule.forRoot(),
     LayersServiceModule.forRoot(),
     OlMapModule,
-    MapcontrolModule
+    MapcontrolModule,
+    MapnavigatorModule
   ],
   providers: [AppStoreService],
   bootstrap: [UkisComponent]
