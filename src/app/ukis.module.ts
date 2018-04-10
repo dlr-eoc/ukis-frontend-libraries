2a5b557eb221f81687a843c5ef89d8761dee19c3
boec_mt <Mathias.Boeck@dlr.de>
2018-04-10 09:58:16 +0200

    update clarity t6o 0.11.12 UKISDEV-430

--- src/app/ukis.module.ts
+++ src/app/ukis.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ClarityModule } from 'clarity-angular';
+import { ClarityModule } from '@clr/angular';
 import { FormsModule } from '@angular/forms';
 
 import { UkisComponent } from './ukis.component';
