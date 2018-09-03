5e6e16fc43333ce093e72cbb8eb0f7e8706ed718
boec_mt <Mathias.Boeck@dlr.de>
2018-09-03 12:59:58 +0200

    add component for privacy

---
+++ src/app/route-components/route-privacy/route-privacy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ukis-route-privacy',
+  templateUrl: './route-privacy.component.html',
+  styleUrls: ['./route-privacy.component.scss']
+})
+export class RoutePrivacyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
