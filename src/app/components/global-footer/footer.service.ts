a4fe881b3d0429af6763f4054ef021481e031e54
boec_mt <Mathias.Boeck@dlr.de>
2018-09-03 12:11:06 +0200

    create component with service for footer

---
+++ src/app/components/global-footer/footer.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FooterService {
+  private showFooter = new Subject<boolean>();
+  footer$ = this.showFooter.asObservable();
+  constructor() { }
+  show(show:boolean) {
+    this.showFooter.next(show);
+  }
+}
