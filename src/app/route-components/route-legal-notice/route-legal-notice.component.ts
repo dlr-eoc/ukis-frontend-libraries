ac2782418b8ad86d3b0ac30941481a78c718abfb
boec_mt <Mathias.Boeck@dlr.de>
2018-09-03 12:59:13 +0200

    add component for legal-notice

---
+++ src/app/route-components/route-legal-notice/route-legal-notice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ukis-route-legal-notice',
+  templateUrl: './route-legal-notice.component.html',
+  styleUrls: ['./route-legal-notice.component.scss']
+})
+export class RouteLegalNoticeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
