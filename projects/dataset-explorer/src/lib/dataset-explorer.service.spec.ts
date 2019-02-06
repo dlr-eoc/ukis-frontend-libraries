94280ecc135c0e400121cb0435f975946c7e19e8
lang_m13 <michael.langbein@dlr.de>
2019-02-06 09:07:29 +0100

    created requirements for debugging owc service

--- projects/dataset-explorer/src/lib/dataset-explorer.service.spec.ts
+++ projects/dataset-explorer/src/lib/dataset-explorer.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { DatasetExplorerService } from './dataset-explorer.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { testContext } from './testContext';
+
 
 describe('DatasetExplorerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: DatasetExplorerService = TestBed.get(DatasetExplorerService);
-    expect(service).toBeTruthy();
+  let injector: TestBed;
+  let datasetExplorer: DatasetExplorerService;
+  let httpMock: HttpTestingController;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DatasetExplorerService]
+    });
+    injector = getTestBed();
+    datasetExplorer = injector.get(DatasetExplorerService);
+    httpMock = injector.get(HttpTestingController);
   });
+
+  afterEach(() => {
+    // checking that there are no outstanding requests
+    httpMock.verify();
+  });
+
+
+  it('#getObservations should actually get observations', () => {
+
+    // step 1: setup data
+    const fakeContext = testContext;
+    const url = "testUrl/rest/owc/";
+
+    // step 2: setup answer to http-request
+    const request = httpMock.expectOne(url);
+    request.flush(fakeContext);
+
+    // step 3: call url
+    datasetExplorer.getObservations(url).subscribe((data) => {
+      expect(data).toBe(fakeContext);
+    });
+  });
+
 });
