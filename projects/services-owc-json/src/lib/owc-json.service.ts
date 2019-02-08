2bc6b4c6d1b344e77e0d7db8aeb5e4ba9dc27e79
lang_m13 <michael.langbein@dlr.de>
2019-02-08 11:16:31 +0100

    ...

--- projects/services-owc-json/src/lib/owc-json.service.ts
+++ projects/services-owc-json/src/lib/owc-json.service.ts
@@ -7,7 +7,7 @@
 
 import { Injectable } from '@angular/core';
 import { IOwsContext, IOwsResource, IOwsOffering } from '@ukis/datatypes-owc-json';
-import { ILayerGroupOptions, ILayerOptions } from '@ukis/datatypes-layers';
+import { ILayerGroupOptions, ILayerOptions, IRasterLayerOptions, VectorLayer, RasterLayer } from '@ukis/datatypes-layers';
 
 
 @Injectable({
@@ -175,5 +175,114 @@ export class OwcJsonService {
     return iconUrl;
   }
 
+  createVectorLayerFromOffering(offering: IOwsOffering, observation: IOwsResource) {
+    let legendUrl = this.owcSvc.getLegendUrl(offering);
+    let iconUrl = this.owcSvc.getIconUrl(offering);
+   
+
+    let layerUrl = offering.operations[0].href;
+    let params = this.getJsonFromUrl(offering.operations[0].href);
+    let lName = params['typeName'];
+    let layeroptins = this.createVectorLayer(lName, offering, observation, layerUrl, legendUrl, iconUrl)
+    let layer = new VectorLayer(layeroptins);
+    if (observation.bbox) {
+      layer.bbox = <[number, number, number, number]>observation.bbox;
+    }
+    this.http.get(layerUrl).pipe(map((response: IOwsContext) => response)).subscribe(data => {
+      layer.data = data;
+
+      //this.layersSvc.updateLayerGroup(layerGroup, true);
+    });
+    return layer;
+  }
+
+
+
+  createRasterLayerFromOffering(offering: IOwsOffering, observation: IOwsResource) {
+    let legendUrl = this.getLegendUrl(offering);
+    let iconUrl = this.getIconUrl(offering);
+
+    //let layerUrl = observation.id + offering.operations[0].href.substr(0, offering.operations[0].href.lastIndexOf("?"));
+    let layerUrl = offering.operations[0].href.substr(0, offering.operations[0].href.lastIndexOf("?"));
+    let urlParams = this.getJsonFromUrl(offering.operations[0].href);
+   
+    
+
+    let layeroptions = this.createRasterLayer(urlParams, offering, layerUrl, observation, legendUrl);
+    let layer = new RasterLayer(layeroptions);
+    if (observation.bbox) {
+      layer.bbox = <[number, number, number, number]>observation.bbox;
+    }
+    return layer;
+
+  }
+
+  private createVectorLayer(lName, offering: IOwsOffering, observation, layerUrl?, legendUrl?, iconUrl?, isActive?) {
+    let layeroptions = new VectorLayer({
+      name: observation.properties.title,
+      id: observation.id,
+      displayName: this.getDisplayName(offering, observation),
+      visible: this.isActive(observation),
+      type: 'geojson',
+      removable: true,
+      attribution: '&copy, <a href="//geoservice.dlr.de/eoc/basemap/">DLR</a>',
+      continuousWorld: false,
+      opacity: 1
+    });
+
+    if (layerUrl) {
+      layeroptions.url = layerUrl;
+    }
+    if (legendUrl) {
+      layeroptions.legendImg = legendUrl;
+    }
+   
+    return layeroptions;
+  }
+
+
+  private createRasterLayer(urlParams, offering: IOwsOffering, layerUrl, observation, legendUrl?) {
+    let rasterOptions: IRasterLayerOptions = {
+      name: observation.properties.title,
+      id: observation.id,
+      displayName: this.getDisplayName(offering, observation),
+      visible: this.isActive(observation),
+      type: 'wms',
+      removable: true,
+      params: {
+        layers: urlParams['LAYERS'],
+        format: urlParams['FORMAT'],
+        time: urlParams['TIME'],
+        version: urlParams['VERSION'],
+        tiled: urlParams['TILED'],
+        transparent: true
+        
+      },
+      url: layerUrl,
+      attribution: '&copy, <a href="dlr.de/eoc">DLR</a>',
+      continuousWorld: false,
+      legendImg: legendUrl,
+      opacity: 1
+    }
+   
+    return rasterOptions;
+  }
+
+
+
+  /** Misc --------------------------------------------------- */
+  /**
+   * helper to pack parameter of a url into a JSON
+   * @param url any url with parameter
+   */
+  getJsonFromUrl(url: string) {
+    var query = url.substr(url.lastIndexOf("?") + 1);
+    var result = {};
+    query.split("&").forEach(function (part) {
+      var item = part.split("=");
+      result[item[0].toUpperCase()] = decodeURIComponent(item[1]);
+    });
+    return result;
+  }
 
 }
