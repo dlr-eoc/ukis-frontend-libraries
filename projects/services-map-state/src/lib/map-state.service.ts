cd2e920286d417b31749ab908758ca1ad3ad5b45
boec_mt <Mathias.Boeck@dlr.de>
2019-02-22 17:13:45 +0100

    adjust mapstate to get state value and add extent, time UKISDEV-695

--- projects/services-map-state/src/lib/map-state.service.ts
+++ projects/services-map-state/src/lib/map-state.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from "rxjs";
-//import { filter, map } from 'rxjs/operators';
-import { MapState, TGeoExtent } from '@ukis/datatypes-map-state';
+import { filter, map } from 'rxjs/operators';
+import { MapState, TGeoExtent, IMapStateOptions, IMapState } from '@ukis/datatypes-map-state';
+import { state } from '@angular/animations';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapStateService {
-  private mapState = new BehaviorSubject(new MapState(12, { lat: 0, lon: 0 }));
+  private mapState = new BehaviorSubject(new MapState(12, { lat: 0, lon: 0 }, null, [-180, -90, 180, 90], null));
   private extent = new BehaviorSubject<TGeoExtent>([-180, -90, 180, 90]);
-
   constructor() {
     //let extent = [-180, -90, 180, 90];
     //this.extent.next(extent);
@@ -17,19 +17,34 @@ export class MapStateService {
     this.mapState.next(state);
   }
 
-  public getMapState(): Observable<MapState> {
-    return this.mapState.asObservable();
+
+  //Observable<MapState> | 
+  public getMapState(): BehaviorSubject<MapState> {
+    return this.mapState;
   }
+
+  /*
+  public setMapState(options: IMapState) {
+    let state = new MapState(options.zoom, options.center, options.options, options.extent, options.time);
+    this.mapState.next(state);
+  }
+  */
 
   public setMapState(state: MapState) {
     this.mapState.next(state);
   }
 
+
+  //Observable<TGeoExtent>
   public getExtent(): Observable<TGeoExtent> {
+    //return this.mapState.pipe(map((state) => { return state.extent }));
     return this.extent.asObservable();
   }
 
   public setExtent(extent: TGeoExtent) {
-    this.extent.next(extent);
+    //let state = this.mapState.getValue();
+    //state.extent = extent;
+    //this.mapState.next(state);
+    this.extent.next(extent)
   }
 }
