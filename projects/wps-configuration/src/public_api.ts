c41d2279f5e8f4b6a1ac3b65bf936d7e040991c9
Langbein, Michael <Michael.Langbein@dlr.de>
2019-04-11 15:50:15 +0200

    deleted wps-configuration

--- projects/wps-configuration/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of wps-configuration
- */
-
-export * from './lib/wps-configuration.service';
-export * from './lib/wps-configuration.component';
-export * from './lib/wps-configuration.module';
