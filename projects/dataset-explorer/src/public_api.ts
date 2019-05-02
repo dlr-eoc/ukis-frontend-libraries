4a7c3eced081982f049e506b7ecc4d0111ea60ef
Keim, Stefan <Stefan.Keim@dlr.de>
2019-05-02 11:04:58 +0200

    UKISDEV-789

--- projects/dataset-explorer/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of observation-explorer
- */
-
-export * from './lib/dataset-explorer.service';
-export * from './lib/dataset-explorer.component';
-export * from './lib/date-range/date-range.component';
-export * from './lib/dataset-explorer.module';
