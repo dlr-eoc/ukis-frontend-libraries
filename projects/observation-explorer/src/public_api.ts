82fc7967158d1efbcdde9ae7a5cc62beb4f5c3a6
boec_mt <Mathias.Boeck@dlr.de>
2019-05-06 17:09:29 +0200

    remove observation-explorer UKISDEV-790

--- projects/observation-explorer/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of observation-explorer
- */
-
-export * from './lib/observation-explorer.service';
-export * from './lib/observation-explorer.component';
-export * from './lib/observation-explorer.module';
