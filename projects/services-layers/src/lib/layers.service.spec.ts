8e360b980e01999ce451458074fdc9521674c5ab
boec_mt <Mathias.Boeck@dlr.de>
2019-05-08 17:33:32 +0200

    create tests for layers-service and fix some bugs found with the tests UKISDEV-799

--- projects/services-layers/src/lib/layers.service.spec.ts
+++ projects/services-layers/src/lib/layers.service.spec.ts
@@ -1,10 +1,67 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RasterLayer } from './types/Layers';
+import { RasterLayer, Layer } from './types/Layers';
 import { LayerGroup } from './types/LayerGroup';
 import { LayersService } from './layers.service';
+import { first } from 'rxjs/operators';
+
+
+let layer1, layer2, layergroup1, layer3, layer4;
+
 
 describe('LayersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    // create test layers
+    layer1 = new RasterLayer({
+      name: 'test layer1',
+      type: 'wms',
+      id: 'layer1',
+      url: '//geoservice.dlr.de/eoc/basemap/wms',
+      removable: true,
+      params: {
+        layers: 'litemap'
+      }
+    });
+
+    layer2 = new RasterLayer({
+      name: 'test layer2',
+      type: 'wms',
+      id: 'layer2',
+      url: '//geoservice.dlr.de/eoc/basemap/wms',
+      removable: true,
+      params: {
+        layers: 'litemap'
+      }
+    });
+    layergroup1 = new LayerGroup({
+      name: 'baslayer Group',
+      filtertype: 'Overlays',
+      id: 'baslayer_group',
+      layers: [layer1]
+    });
+
+    layer3 = new RasterLayer({
+      name: 'test layer3',
+      type: 'wms',
+      id: 'layer3',
+      url: '//geoservice.dlr.de/eoc/basemap/wms',
+      removable: true,
+      params: {
+        layers: 'litemap'
+      }
+    });
+
+    layer4 = new RasterLayer({
+      name: 'test layer4',
+      type: 'wms',
+      id: 'layer4',
+      url: '//geoservice.dlr.de/eoc/basemap/wms',
+      removable: true,
+      params: {
+        layers: 'litemap'
+      }
+    });
+  });
 
   it('should be created', () => {
     const service: LayersService = TestBed.get(LayersService);
@@ -13,111 +70,171 @@ describe('LayersService', () => {
 
   it('should add a layer to BaseLayers', async(() => {
     const service: LayersService = TestBed.get(LayersService);
-
-    let layer = new RasterLayer({
-      name: 'test layer',
-      type: 'wms',
-      id: 'test_layer',
-      url: '//geoservice.dlr.de/eoc/basemap/wms',
-      params: {
-        layers: 'litemap'
-      }
-    });
-
-    service.addLayer(layer, 'Baselayers');
-
+    service.addLayer(layer1, 'Baselayers');
     service.getBaseLayers().subscribe((layers) => {
-      expect(layers.length).toEqual(1)
-    })
+      expect(layers.length).toEqual(1);
+    });
   }));
 
   it('should remove a layer from BaseLayers', async(() => {
     const service: LayersService = TestBed.get(LayersService);
+    service.addLayer(layer1, 'Baselayers');
+    expect(service.getBaseLayersCount()).toEqual(1);
 
-    let layer = new RasterLayer({
-      name: 'test layer',
-      type: 'wms',
-      id: 'test_layer',
-      url: '//geoservice.dlr.de/eoc/basemap/wms',
-      params: {
-        layers: 'litemap'
-      }
+    service.removeLayer(layer1, 'Baselayers');
+    expect(service.getBaseLayersCount()).toEqual(0);
+  }));
+
+  it('should remove a layer by ID', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    service.addLayer(layer1, 'Overlays');
+    service.removeLayer(layer1, 'Overlays');
+    expect(service.getOverlaysCount()).toEqual(0);
+  }));
+
+  it('should add a layer to Layers', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    service.addLayer(layer1, 'Layers');
+
+    service.getLayers().subscribe((layers) => {
+      expect(layers.length).toEqual(1);
     });
-    service.addLayer(layer, 'Baselayers');
-    expect(service.getBaseLayersCount()).toEqual(1)
+  }));
 
-    service.removeLayer(layer, 'Baselayers')
-    expect(service.getBaseLayersCount()).toEqual(0)
+  it('should remove a layer from Layers', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+
+    service.addLayer(layer1, 'Layers');
+    expect(service.getLayersCount()).toEqual(1);
+
+    service.removeLayer(layer1, 'Layers');
+    expect(service.getLayersCount()).toEqual(0);
   }));
 
   it('should add a layer to Overlays', async(() => {
     const service: LayersService = TestBed.get(LayersService);
-
-    let layer = new RasterLayer({
-      name: 'test layer',
-      type: 'wms',
-      id: 'test_layer',
-      url: '//geoservice.dlr.de/eoc/basemap/wms',
-      params: {
-        layers: 'litemap'
-      }
-    });
-
-    service.addLayer(layer, 'Overlays');
-
+    service.addLayer(layer1, 'Overlays');
     service.getOverlays().subscribe((layers) => {
-      expect(layers.length).toEqual(1)
-    })
+      expect(layers.length).toEqual(1);
+    });
   }));
 
   it('should remove a layer from Overlays', async(() => {
     const service: LayersService = TestBed.get(LayersService);
+    service.addLayer(layer1, 'Overlays');
+    expect(service.getOverlaysCount()).toEqual(1);
 
-    let layer = new RasterLayer({
-      name: 'test layer',
-      type: 'wms',
-      id: 'test_layer',
-      url: '//geoservice.dlr.de/eoc/basemap/wms',
-      params: {
-        layers: 'litemap'
-      }
-    });
-    service.addLayer(layer, 'Overlays');
-    expect(service.getOverlaysCount()).toEqual(1)
-
-    service.removeLayer(layer, 'Overlays')
-    expect(service.getOverlaysCount()).toEqual(0)
+    service.removeLayer(layer1, 'Overlays');
+    expect(service.getOverlaysCount()).toEqual(0);
   }));
 
 
-  it('should add a layerGroup', async(() => {
+  it('should add a layerGroup to Layers', async(() => {
     const service: LayersService = TestBed.get(LayersService);
-
-    let layergroup = new LayerGroup({
-      name: 'baslayer Group',
-      //type: 'baselayers',
-      id: 'baslayer_group',
-      layers: []
-    });
-
-    service.addLayerGroup(layergroup)
-    expect(service.getLayerGroupsCount()).toEqual(1)
-    //clean up
-    service.removeLayerOrGroupById('baslayer_group')
+    layergroup1.filtertype = 'Layers';
+    service.addLayerGroup(layergroup1);
+    expect(service.getLayersCount()).toEqual(1);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+    // clean up
+    service.removeLayerGroup(layergroup1);
+    expect(service.getLayersCount()).toEqual(0);
   }));
 
   it('should remove a layerGroup by ID', async(() => {
     const service: LayersService = TestBed.get(LayersService);
-    let layergroup = new LayerGroup({
-      name: 'removable Group',
-      //type: 'Overlays',
-      id: 'removable_group',
-      layers: []
-    });
-    service.addLayerGroup(layergroup)
-
-    service.removeLayerOrGroupById('removable_group')
-
-    expect(service.getLayerGroupsCount()).toEqual(0)
+    layergroup1.id = 'removable_group';
+    service.addLayerGroup(layergroup1);
+    service.removeLayerOrGroupById('removable_group');
+    expect(service.getLayerGroupsCount()).toEqual(0);
   }));
+
+  it('should not remove a non removable layerGroup', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    layergroup1.removable = false;
+    service.addLayerGroup(layergroup1);
+    service.removeLayerGroup(layergroup1);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+  }));
+
+  it('should add a layerGroup to Overlays', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    layergroup1.filtertype = 'Overlays';
+    service.addLayerGroup(layergroup1);
+    expect(service.getOverlaysCount()).toEqual(1);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+    // clean up
+    service.removeLayerGroup(layergroup1);
+    expect(service.getOverlaysCount()).toEqual(0);
+  }));
+
+  it('should remove a layer from a layerGroup', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    service.addLayerGroup(layergroup1);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+    expect(service.getOverlaysCount()).toEqual(1);
+    // clean up
+    service.removeLayerFromGroup(layer1, layergroup1, false);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+    expect(service.getOverlaysCount()).toEqual(0);
+  }));
+
+  it('should remove a layer by Id from a layerGroup', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    service.addLayerGroup(layergroup1);
+    expect(service.getLayerGroupsCount()).toEqual(1);
+    expect(service.getOverlaysCount()).toEqual(1);
+    // clean up;
+    service.removeLayerGroup(layergroup1);
+    expect(service.getLayerGroupsCount()).toEqual(0);
+    expect(service.getOverlaysCount()).toEqual(0);
+  }));
+
+  it('should move group or layer up and down in the array', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    /** layer2 == 0, layer1 == 1, layer3 == 2  */
+    const layers = [layer2, layergroup1, layer3];
+
+    layers.map(layer => {
+      if (layer instanceof Layer) {
+        service.addLayer(layer, 'Layers');
+      } else if (layer instanceof LayerGroup) {
+        service.addLayerGroup(layer);
+      }
+    });
+
+    /** layer2 == 1 */
+
+    service.setGroupLayerIndex(layer2, 'down');
+    service.getLayerGroups().pipe(first()).subscribe(groups => {
+      const flatt = service.flattenDeepArray(groups);
+      expect(flatt[0]).toBe(layer1);
+      expect(flatt[1]).toBe(layer2);
+      expect(flatt[2]).toBe(layer3);
+    });
+  }));
+
+  //setLayerGroups(this.layergroups);
+  /*
+
+  it('should set correct zIndex for groups or layers', async(() => {
+    const service: LayersService = TestBed.get(LayersService);
+    const layers = [layer2, layergroup1, layer3];
+
+    service.setLayerGroups(layers);
+    //service.arrayMove()
+    service.getLayerGroups().pipe(first()).subscribe(groups => {
+      const flatt = service.flattenDeepArray(groups);
+      console.log(flatt);
+      expect(flatt[0]).toBe(layer2);
+      expect(flatt[1]).toBe(layer1);
+      expect(flatt[2]).toBe(layer3);
+    });
+  }));
+
+
+  it('should set correct zIndex in a group', async(() => {
+    //setLayerIndexInGroup
+  }));
+*/
+
 });
