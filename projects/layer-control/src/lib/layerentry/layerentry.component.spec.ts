21b0a13cfe35f3175cac22cb71915f25a89613d8
boec_mt <Mathias.Boeck@dlr.de>
2019-10-28 12:41:42 +0100

    add test for layerentry inputs

--- projects/layer-control/src/lib/layerentry/layerentry.component.spec.ts
+++ projects/layer-control/src/lib/layerentry/layerentry.component.spec.ts
@@ -5,29 +5,33 @@ import { ClarityModule } from '@clr/angular';
 import { FormsModule } from '@angular/forms';
 import { Layer, LayerGroup } from '@ukis/services-layers';
 import { LayersService } from '@ukis/services-layers';
+import { MapStateService } from '@ukis/services-map-state';
 
 describe('LayerentryComponent', () => {
   let component: LayerentryComponent;
   let fixture: ComponentFixture<LayerentryComponent>;
+  let layersSvc: LayersService;
+  let mapStateSvc: MapStateService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ClarityModule, FormsModule],
       declarations: [LayerentryComponent],
-      providers: [LayersService]
-    })
-      .compileComponents();
+      providers: [LayersService, MapStateService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LayerentryComponent);
     component = fixture.componentInstance;
-    component.layersSvc = new LayersService();
+    layersSvc = TestBed.get(LayersService);
+    mapStateSvc = TestBed.get(MapStateService);
+
     component.layer = new Layer({
       type: 'wms',
       name: 'test layer',
       id: 'test_layer'
-    })
+    });
 
     component.group = new LayerGroup({
       filtertype: 'Overlays',
@@ -35,11 +39,49 @@ describe('LayerentryComponent', () => {
       name: 'Overlays',
       layers: [component.layer]
 
-    })
+    });
+
+    component.layerGroups = [component.group];
+    component.layersSvc = layersSvc;
+    component.mapState = mapStateSvc;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have input layersSvc', () => {
+    fixture.detectChanges();
+    expect(component.layersSvc instanceof LayersService).toBeTruthy();
+  });
+
+  it('should have input mapState', () => {
+    fixture.detectChanges();
+    expect(component.mapState instanceof MapStateService).toBeTruthy();
+  });
+
+  it('should have input layer', () => {
+    expect(component.layer instanceof Layer).toBeTruthy();
+  });
+
+  it('should have input group', () => {
+    expect(component.group instanceof LayerGroup).toBeTruthy();
+  });
+
+  it('should have input layerGroups', () => {
+    expect(component.layerGroups[0] instanceof LayerGroup).toBeTruthy();
+  });
+
+  it('should have input expanded', () => {
+    component.openProperties = true;
+    fixture.detectChanges();
+    expect(component.openProperties).toBe(true);
+  });
+
+  it('should have input expandable', () => {
+    component.expandable = true;
+    fixture.detectChanges();
+    expect(component.expandable).toBe(true);
   });
 });
