d332a5e508266c1ec5be994bc40081e8777515d1
boec_mt <Mathias.Boeck@dlr.de>
2019-11-19 15:18:01 +0100

    place component in folder

---
+++ projects/map-navigator/src/lib/navigator/map-navigator.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MapStateService } from '@ukis/services-map-state';
+import { MapState } from '@ukis/services-map-state';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'ukis-map-navigator',
+  templateUrl: './map-navigator.component.html',
+  //styleUrls: ['./map-navigator.component.scss']
+  styles: []
+})
+export class MapNavigatorComponent implements OnInit {
+  @Input('mapState') mapState?: MapStateService;
+  mapstate: MapState;
+  subscription: Subscription;
+  constructor() { }
+
+  ngOnInit() {
+    this.subscription = this.mapState.getMapState().subscribe(mapstate => { this.mapstate = mapstate; });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  zoomIn() {
+    this.mapstate.zoom++;
+    this.setNewState(this.mapstate);
+  }
+  zoomOut() {
+    this.mapstate.zoom--;
+    this.setNewState(this.mapstate);
+  }
+
+  setNewState(newstate: MapState) {
+    newstate.options.notifier = 'user';
+    this.mapState.setMapState(newstate);
+  }
+
+  stateChange(ev) {
+    this.setNewState(this.mapstate);
+  }
+
+}
