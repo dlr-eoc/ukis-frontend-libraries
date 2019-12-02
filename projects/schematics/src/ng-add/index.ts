ded8181f075dee58a4b58bd2642b4d46b40d801b
boec_mt <Mathias.Boeck@dlr.de>
2019-12-02 16:33:27 +0100

    adjust add schematic

--- projects/schematics/src/ng-add/index.ts
+++ projects/schematics/src/ng-add/index.ts
@@ -1,36 +1,64 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-// import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import { Rule, SchematicContext, Tree, apply, url, chain, noop } from '@angular-devkit/schematics';
+import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
 import { updateTsConfigPaths } from '../utils';
+
+// https://nitayneeman.com/posts/making-an-addable-angular-package-using-schematics/
+
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function ngAdd(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+export function ngAdd(options: any): Rule {
+  /* return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  }; */
 
-    const tsconfigPaths = [
-      { name: '@ukis/*', paths: ['frontend-libraries/projects/*'] }
-    ];
+  return chain([
+    options && options.skipPackageJson ? noop() : addPackageJsonDependencies(),
+    options && options.skipTsConfigJson ? noop() : updateTsConfig(),
+    options && options.skipAddFiles ? noop() : addFiles()
+    // options && options.skipPackageJson ? noop() : installPackageJsonDependencies(),
+    //options && options.skipModuleImport ? noop() : addModuleToImports(options),
+    // options && options.skipPolyfill ? noop() : addPolyfillToScripts(options)
+  ]);
+}
 
-    tsconfigPaths.forEach((path) => {
-      updateTsConfigPaths(path.name, path.paths)
-      _context.logger.log('info', `✅️ Added "${path.name}" with paths "${path.paths.join(',')}" into tsconfig.json`);
-    });
-
-    //const packageJson = JSON.parse(tree.read('package.json')!.toString('utf8'));
-    //console.log(packageJson);
-    /* const version = '~2.0.0';
+function addPackageJsonDependencies(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const version = '~2.0.0';
     const dependencies: NodeDependency[] = [
       { type: NodeDependencyType.Default, version: version, name: '@clr/angular' },
       { type: NodeDependencyType.Default, version: version, name: '@clr/ui' },
       { type: NodeDependencyType.Default, version: version, name: '@clr/icons' },
       { type: NodeDependencyType.Default, version: version, name: '@clr/core' }
     ];
-    dependencies.forEach((dep) => {
-      addPackageJsonDependency(tree, dep);
-      _context.logger.log('info', `✅️ Added "${dep.name}" into ${dep.type}`);
-    }); */
 
+    dependencies.forEach(dependency => {
+      addPackageJsonDependency(host, dependency);
+      context.logger.log('info', `✅️ Added "${dependency.name}" into ${dependency.type}`);
+    });
 
-    return tree;
+    return host;
+  };
+}
+
+function addFiles(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    apply(url('./files'), []);
+    context.logger.log('info', `Added files`);
+    return host;
+  };
+}
+
+function updateTsConfig(): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const tsconfigPaths = [
+      { name: '@ukis/*', paths: ['frontend-libraries/projects/*'] }
+    ];
+
+    tsconfigPaths.forEach((path) => {
+      updateTsConfigPaths(path.name, path.paths)
+      context.logger.log('info', `✅️ Added "${path.name}" with paths "${path.paths.join(',')}" into tsconfig.json`);
+    });
+    return host;
   };
 }
