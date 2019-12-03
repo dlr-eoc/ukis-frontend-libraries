b9bcf8a35039ba7a7995920d24c9959c03b4b027
boec_mt <Mathias.Boeck@dlr.de>
2019-12-03 12:00:23 +0100

    add progress and icons

--- projects/core-ui/src/lib/core-ui.module.ts
+++ projects/core-ui/src/lib/core-ui.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CoreUiComponent } from './core-ui.component';
 import { GlobalAlertComponent } from './global-alert/global-alert.component';
 import { AlertService } from './global-alert/alert.service';
-
+import { GlobalProgressComponent } from './global-progress/global-progress.component';
+import { ProgressService } from './global-progress/progress.service';
+import { UkisShapes } from './icons/ukis';
 
 
 @NgModule({
-  declarations: [CoreUiComponent, GlobalAlertComponent],
+  declarations: [CoreUiComponent, GlobalAlertComponent, GlobalProgressComponent],
   imports: [],
-  exports: [CoreUiComponent, GlobalAlertComponent],
-  providers: [AlertService]
+  exports: [CoreUiComponent, GlobalAlertComponent, GlobalProgressComponent, UkisShapes],
+  providers: [AlertService, ProgressService]
 })
 export class CoreUiModule { }
--- projects/core-ui/src/public-api.ts
+++ projects/core-ui/src/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of core-ui
  */
 
+export * from './lib/global-progress/global-progress.component';
+export * from './lib/global-progress/progress.service';
+export * from './lib/global-alert/global-alert.component';
+export * from './lib/global-alert/alert.service';
 export * from './lib/core-ui.component';
 export * from './lib/core-ui.module';
