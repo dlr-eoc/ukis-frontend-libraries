1bd53e2a3a0362b5c311463eccd1951e33862b12
boec_mt <Mathias.Boeck@dlr.de>
2019-12-03 11:44:14 +0100

    add alert component to core-ui

--- projects/core-ui/src/lib/core-ui.module.ts
+++ projects/core-ui/src/lib/core-ui.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CoreUiComponent } from './core-ui.component';
+import { GlobalAlertComponent } from './global-alert/global-alert.component';
+import { AlertService } from './global-alert/alert.service';
 
 
 
 @NgModule({
-  declarations: [CoreUiComponent],
-  imports: [
-  ],
-  exports: [CoreUiComponent]
+  declarations: [CoreUiComponent, GlobalAlertComponent],
+  imports: [],
+  exports: [CoreUiComponent, GlobalAlertComponent],
+  providers: [AlertService]
 })
 export class CoreUiModule { }
