45c9f45c4512915dde585ef61132e3a26d3e494e
boec_mt <Mathias.Boeck@dlr.de>
2019-12-03 12:00:52 +0100

    add exports

--- projects/core-ui/src/public-api.ts
+++ projects/core-ui/src/public-api.ts
@@ -6,5 +6,7 @@ export * from './lib/global-progress/global-progress.component';
 export * from './lib/global-progress/progress.service';
 export * from './lib/global-alert/global-alert.component';
 export * from './lib/global-alert/alert.service';
+
+export * from './lib/icons/ukis';
 export * from './lib/core-ui.component';
 export * from './lib/core-ui.module';
