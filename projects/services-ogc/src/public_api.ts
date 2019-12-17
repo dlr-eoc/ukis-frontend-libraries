11c00576d7319879531236b2660eca8424d2cb99
Langbein <Michael.Langbein@dlr.de>
2019-12-17 10:18:52 +0100

    services-ogc: made wps-datatypes available via public api

--- projects/services-ogc/src/public_api.ts
+++ projects/services-ogc/src/public_api.ts
@@ -3,4 +3,5 @@ export * from './lib/owc/owc-json.service';
 export * from './lib/owc/types/owc-json';
 export * from './lib/owc/types/eoc-owc-json';
 export * from './lib/wps/wpsclient';
+export * from './lib/wps/wps_datatypes';
 export * from './lib/wmts/wmtsclient.service';
