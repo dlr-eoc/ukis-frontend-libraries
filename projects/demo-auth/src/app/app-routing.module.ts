a52b9b748d7a49964d8ef2a5c36b0bb4521224a9
boec_mt <Mathias.Boeck@dlr.de>
2020-02-11 09:18:29 +0100

    fix demo auth

--- projects/demo-auth/src/app/app-routing.module.ts
+++ projects/demo-auth/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { RouteLoginComponent } from './route-components/route-login/route-login.component';
 import { RouteMapComponent } from './route-components/route-map/route-map.component';
 import { AuthGuardService } from './auth/auth-guard.service';
@@ -24,7 +24,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
