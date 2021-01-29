03c9002ad8a936dd43385c9133801bc6bce15c9d
Langbein, Michael <michael.langbein@dlr.de>
2021-01-29 12:13:22 +0100

    refactor: removing instrumentation from TablePopupComponent since it's no longer required.

--- projects/demo-maps/src/app/components/table-popup/table-popup.component.ts
+++ projects/demo-maps/src/app/components/table-popup/table-popup.component.ts
@@ -1,29 +1,16 @@
-import { Component, OnInit, Input, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-popup',
   templateUrl: './table-popup.component.html',
   styleUrls: ['./table-popup.component.scss']
 })
-export class TablePopupComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class TablePopupComponent implements OnInit {
 
   @Input() data: any;
-  public id: number;
 
   constructor() {
-    this.id = Math.random() * 1000000;
-    console.log(`table-popup constructed with id ${this.id}`);
   }
 
-  ngOnInit(): void {
-    // console.log(`table-popup on-init with id ${this.id}`);
-  }
-
-  ngAfterViewChecked(): void {
-    console.log(`checking table-popup with id ${this.id}`);
-  }
-
-  ngOnDestroy(): void {
-    console.log(`destroying table-popup with id ${this.id}`);
-  }
+  ngOnInit(): void {}
 }
