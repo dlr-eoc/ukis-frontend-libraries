2fe1b281cc88e2583dd0f4ea87f437918c93f961
Mathias Boeck <Mathias.Boeck@dlr.de>
2021-05-03 12:05:52 +0200

    ci: update check branch action #79

--- .github/actions/check-branch-contains/index.sh
+++ .github/actions/check-branch-contains/index.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
-# Check versions of package.json, tag and npm
-# Check if the latest git Tag is the same as the repo npm version
-# Check if the repo version is not in the versions published on on npm
+# Check if commit of Tag which is pushed, is already in the main remote branch.
+# This should be true if the version was set by [npm-version](https://docs.npmjs.com/cli/v6/commands/npm-version)
+# And the commit is merged in the main branch by a pull request
 
 # Usage
 # chmod +x index.sh or git update-index --chmod=+x index.sh
-#MAIN_BRANCH="origin/master"
+# ----------------------------------------------------------
+#BRANCH="origin/main"
+#COMMITID="latestTag"
+#COMMITID="refs/tags/v0.9.0"
 
+# https://stackoverflow.com/questions/10649814/get-last-git-tag-from-a-remote-repo-without-cloning
+# https://git-scm.com/docs/git-ls-remote
+# get tags from remote git sorted by version and only show last row with commit stripped of
+#latestTagOnRemote=$(git ls-remote --tags --refs --sort="version:refname" origin | awk -F/ 'END{print$NF}')
+#latestTagOnRemote=$(git ls-remote --tags --refs --sort="version:refname" origin | tail -n1 | sed 's/.*\///; s/v//g')
 
-# get versions from git
-latestTagOrig=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-latestTag="$(printf "%s" "$latestTagOrig" | sed -e "s/v//g")"
-if [ ! "${latestTagOrig}" ];then
-  echo "no git tag:${latestTagOrig}"
-  exit 1
+# get commit or latest tag
+if [ "$COMMITID" == 'latestTag' ];then
+  # get latest tag in fetched repo
+  # https://git-scm.com/docs/git-rev-list
+  # https://git-scm.com/docs/git-describe
+  # get latest commit for tags in the fetched repo and print it as tag
+  COMMITID=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+  if [ ! "${COMMITID}" ];then
+    echo "no git tag:${COMMITID}"
+    exit 1
+  fi
+else
+  COMMITID=$(printf "%s" "$COMMITID" | sed 's/refs\///g; s/tags\///g;')
 fi
 
-# check if commit/tag is in remote master branch
-# befor run git remote prune github
-remoteBranchContains="$(git branch -r --contains "$(git rev-parse v"$latestTag")" )"
-commitInMainBranch="$(printf "%s" "$remoteBranchContains" | grep "$MAIN_BRANCH")"
+echo "tag to check:${COMMITID}"
 
-if [ ! "$commitInMainBranch" ];then
-  echo "v${latestTag} not in ${MAIN_BRANCH}"
+
+
+# Check if commit/tag is in main remote branch
+# https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---containsltcommitgt
+# https://git-scm.com/docs/git-rev-parse
+branchesWhichContain="$(git branch -r --contains "$(git rev-parse "$COMMITID")" )"
+# Check is in branch
+commitInBranch="$(printf "%s" "$branchesWhichContain" | grep "$BRANCH")"
+
+if [ ! "$commitInBranch" ];then
+  echo "${COMMITID} not in ${BRANCH}"
   exit 1
+else
+  echo "${COMMITID} is in ${BRANCH}"
+  exit 0
 fi
