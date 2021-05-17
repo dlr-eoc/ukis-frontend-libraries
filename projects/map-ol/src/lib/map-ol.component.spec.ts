7542466fc7c7c1d010d13df365a1c1f8e58d9159
Langbein, Michael <michael.langbein@dlr.de>
2021-05-17 15:10:35 +0200

    fix: custom-layers containing olLayerGroups get assigned a sensible z-index

--- projects/map-ol/src/lib/map-ol.component.spec.ts
+++ projects/map-ol/src/lib/map-ol.component.spec.ts
@@ -3,15 +3,20 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MapOlComponent } from './map-ol.component';
 import { LayersService, VectorLayer } from '@dlr-eoc/services-layers';
 import { RasterLayer } from '@dlr-eoc/services-layers';
-import { LayerGroup } from '@dlr-eoc/services-layers';
+import { LayerGroup, CustomLayer } from '@dlr-eoc/services-layers';
+import { OsmTileLayer } from '@dlr-eoc/base-layers-raster';
 import { of } from 'rxjs';
 import { MapStateService } from '@dlr-eoc/services-map-state';
 import { Injectable } from '@angular/core';
 import testFeatureData from '../assets/testFeatureCollection.json';
 import olVectorLayer from 'ol/layer/Vector';
+import olLayerGroup from 'ol/layer/Group';
+import olVectorSource from 'ol/source/Vector';
+import olGeoJSON from 'ol/format/GeoJSON';
+import { MapOlService } from './map-ol.service';
 
 /**
- * this service extends the LayersService to mimik its behaviour. The getLayerGroups function is overwritten to
+ * this service extends the LayersService to mimic its behavior. The getLayerGroups function is overwritten to
  * get test data for following tests.
  */
 @Injectable()
@@ -46,6 +51,7 @@ describe('MapOlComponent', () => {
     TestBed.configureTestingModule({
       declarations: [MapOlComponent],
       providers: [
+        MapOlService,
         { provide: LayersService, useClass: MockLayersService },
         { provide: MapStateService, useClass: MapStateService }
       ]
@@ -104,5 +110,132 @@ describe('MapOlComponent', () => {
     component.layersSvc.updateLayer(vectorLayer);
     // now, after calling `updateLayer`, the data is present on the ol-layer
     expect((component.map.getLayers().getArray()[1].getLayersArray()[0] as olVectorLayer).getSource().getFeatures().length).toEqual(4);
+  });
+
+  it('should use the correct z-index for Ukis-custom-layers even if they contain an olLayerGroup', () => {
+    const service: MapOlService = TestBed.inject(MapOlService);
+    service.createMap();
+
+
+    const osmLayer = new OsmTileLayer();
+    component.layersSvc.addLayer(osmLayer, 'Baselayers');
+    const baseLayer = new VectorLayer({
+      id: 'vectorLayer',
+      name: 'vectorLayer',
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [{
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: [11.71142578125, 46.5739667965278]
+            }
+          }]
+      }
+    });
+    component.layersSvc.addLayer(baseLayer, 'Layers');
+
+    const layer1 = new olVectorLayer({
+      source: new olVectorSource({
+        features: new olGeoJSON({
+          dataProjection: 'EPSG:4326',
+          featureProjection: service.EPSG
+        }).readFeatures({
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "Point",
+                "coordinates": [
+                  10.37109375,
+                  50.064191736659104
+                ]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "Point",
+                "coordinates": [
+                  11.997070312499998,
+                  50.064191736659104
+                ]
+              }
+            }
+          ]
+        })
+      })
+    });
+    const layer2 = new olVectorLayer({
+      source: new olVectorSource({
+        features: new olGeoJSON({
+          dataProjection: 'EPSG:4326',
+          featureProjection: service.EPSG
+        }).readFeatures({
+          "type": "FeatureCollection",
+          "features": [
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                  [
+                    8.67919921875,
+                    49.167338606291075
+                  ],
+                  [
+                    10.458984375,
+                    48.4146186174932
+                  ],
+                  [
+                    11.997070312499998,
+                    48.4146186174932
+                  ],
+                  [
+                    13.24951171875,
+                    48.821332549646634
+                  ],
+                  [
+                    13.7109375,
+                    49.396675075193976
+                  ]
+                ]
+              }
+            }
+          ]
+        })
+      })
+    });
+
+    const layerGroup = new olLayerGroup({
+      layers: [layer1, layer2]
+    });
+
+    const ukisLayer = new CustomLayer({
+      custom_layer: layerGroup,
+      id: 'olGroupLayer',
+      name: 'Ol-Group Layer',
+      filtertype: 'Layers',
+      visible: true
+    });
+
+    component.layersSvc.addLayer(ukisLayer, 'Layers');
+
+    const lowLayer = service.getLayerByKey({key: 'id', value: baseLayer.id});
+    const higherLayer = service.getLayerByKey({key: 'id', value: 'olGroupLayer'});
+    const higherLayersChild = higherLayer.getLayersArray()[0];
+
+    expect(lowLayer.getZIndex()).toBeDefined();
+    expect(higherLayer.getZIndex()).toBeDefined();
+    expect(higherLayersChild.getZIndex()).toBeDefined();
+    expect(lowLayer.getZIndex() <= higherLayer.getZIndex()).toBeTrue();
+    expect(lowLayer.getZIndex() <= higherLayersChild.getZIndex()).toBeTrue();
+    expect(higherLayer.getZIndex() <= higherLayersChild.getZIndex()).toBeTrue();
   });
 });
