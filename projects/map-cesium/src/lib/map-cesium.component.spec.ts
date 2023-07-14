a6d4bf99b91341fcec30ec0d3c5176c3507d5954
Mathias Boeck <Mathias.Boeck@dlr.de>
2023-07-14 08:35:20 +0200

    feat: create tests

--- projects/map-cesium/src/lib/map-cesium.component.spec.ts
+++ projects/map-cesium/src/lib/map-cesium.component.spec.ts
@@ -1,23 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapCesiumComponent } from './map-cesium.component';
+import { ICesiumControls, MapCesiumComponent } from './map-cesium.component';
+import { LayersService } from '@dlr-eoc/services-layers';
+import { MapCesiumService } from './map-cesium.service';
+import { MapStateService } from '@dlr-eoc/services-map-state';
+import { Viewer } from '@cesium/widgets';
+
 
 describe('MapCesiumComponent', () => {
   let component: MapCesiumComponent;
   let fixture: ComponentFixture<MapCesiumComponent>;
+  let mapSvc: MapCesiumService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapCesiumComponent ]
+      declarations: [MapCesiumComponent],
+      providers: [
+        MapCesiumService,
+        LayersService,
+        MapStateService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    const controls: ICesiumControls = {
+      infoBox: true,
+      selectionIndicator: true
+    }
 
     fixture = TestBed.createComponent(MapCesiumComponent);
     component = fixture.componentInstance;
+    component.twoDlayersSvc = new LayersService();
+    component.mapStateSvc = new MapStateService();
+    mapSvc = TestBed.inject(MapCesiumService);
+    component.controls = controls;
+    (window as Record<string, any>)['CESIUM_BASE_URL'] = 'assets/cesium/';
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a viewer and the viewer should be the same as from the mapSvc', () => {
+    expect(component.viewer instanceof Viewer).toBeTruthy();
+    expect(component.viewer === mapSvc.viewer).toBeTruthy();
+  });
+
+  // TODO: add tests for adding layers
 });
--- projects/map-cesium/src/lib/map-cesium.service.spec.ts
+++ projects/map-cesium/src/lib/map-cesium.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MapCesiumService } from './map-cesium.service';
+import { Viewer } from '@cesium/widgets';
+import { ICesiumControls } from './map-cesium.component';
+
+let mapTarget: { size: number[], container: HTMLDivElement };
+
+const createMapTarget = (size: number[]) => {
+  const container = document.createElement('div');
+  container.style.border = 'solid 1px #000';
+  container.style.width = `${size[0]}px`;
+  container.style.height = `${size[1]}px`;
+  document.body.appendChild(container);
+  return {
+    size,
+    container
+  };
+};
 
 describe('MapCesiumService', () => {
   let service: MapCesiumService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    mapTarget = createMapTarget([1024, 768]);
+    (window as Record<string, any>)['CESIUM_BASE_URL'] = 'assets/cesium/';
     service = TestBed.inject(MapCesiumService);
+
+    const controls: ICesiumControls = {
+      infoBox: true,
+      selectionIndicator: true
+    }
+    service.setControls(controls);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create a viewer', () => {
+    service.createMap(mapTarget.container);
+    expect(service.viewer.container).toEqual(mapTarget.container);
+    expect(service.viewer instanceof Viewer).toBeTruthy();
   });
 });
