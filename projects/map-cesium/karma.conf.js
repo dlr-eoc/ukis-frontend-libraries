f5f666b2662a73b2f8cc52c1eaab1fb8e83b7f60
Mathias Boeck <Mathias.Boeck@dlr.de>
2023-07-14 08:32:21 +0200

    feat: serve assets in tests for cesium

--- projects/map-cesium/karma.conf.js
+++ projects/map-cesium/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const PATH = require('path');
 
 module.exports = function (config) {
   config.set({
@@ -12,6 +13,21 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    // // https://karma-runner.github.io/6.3/config/files.html#loading-assets
+    files: [
+      // https://github1s.com/CesiumGS/cesium/blob/main/gulpfile.js#L1396-L1399
+      { pattern: "../../node_modules/@cesium/engine/Build/Workers/**", included: false, served: true },
+      { pattern: "../../node_modules/@cesium/engine/Source/Assets/**", included: false, served: true },
+      { pattern: "../../node_modules/@cesium/engine/Source/ThirdParty/**", included: false, served: true },
+      { pattern: "../../node_modules/@cesium/engine/Source/Widget/*.css", included: false, served: true },
+    ],
+    // https://github.com/karma-runner/karma/issues/2703#issuecomment-421987843
+    proxies: {
+      // see angular.json assets for cesium!
+      '/assets/cesium/': `/absolute${PATH.normalize(PATH.resolve('node_modules/@cesium/engine/Source/'))}`,
+      '/assets/cesium/Widgets/': `/absolute${PATH.normalize(PATH.resolve('node_modules/@cesium/widgets/Source/'))}`,
+      '/assets/cesium/Workers/': `/absolute${PATH.normalize(PATH.resolve('node_modules/@cesium/engine/Build/Workers/'))}`,
+    },
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
