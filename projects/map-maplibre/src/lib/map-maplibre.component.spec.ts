7a0897877b32f75e21d1c0797aa26373c0171810
Mathias Boeck <Mathias.Boeck@dlr.de>
2023-08-23 16:40:37 +0200

    test: add tests for maplibre component

--- projects/map-maplibre/src/lib/map-maplibre.component.spec.ts
+++ projects/map-maplibre/src/lib/map-maplibre.component.spec.ts
@@ -1,23 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapMaplibreComponent } from './map-maplibre.component';
+import { MapMaplibreService } from './map-maplibre.service';
+import { MapStateService } from '@dlr-eoc/services-map-state';
+import { LayersService } from '@dlr-eoc/services-layers';
+import { Map } from 'maplibre-gl';
+import { EocBasemapTile, OsmTileLayer, EocBaseoverlayTile } from '@dlr-eoc/base-layers-raster';
 
+
+function addSomeLayers(component: MapMaplibreComponent, mapSvc: MapMaplibreService) {
+  /* 
+   * Unfortunately, component.subscribeToLayers() does not work in the test, so we have to add the layers manually instead of using layersSvc.
+   */
+  /**
+   * baseLayers.forEach(l => {
+        component.layersSvc.addLayer(l, 'Baselayers');
+      });
+   */
+
+  const layers = [new OsmTileLayer(), new EocBaseoverlayTile(), new EocBasemapTile()];
+  //@ts-ignore use of private function
+  component.addUpdateLayers(layers.filter(l => mapSvc.layerIsSupported(l)), 'Layers');
+
+  // mapSvc.setUkisLayers(baseLayers, 'Baselayers', component.map);
+  return {
+    layers
+  };
+}
+
+/**
+ *  Unfortunately running tests in watch with edit does not work 
+ *  
+ * -> Async function did not complete within 5000ms
+ */
 describe('MapMaplibreComponent', () => {
   let component: MapMaplibreComponent;
   let fixture: ComponentFixture<MapMaplibreComponent>;
+  let mapSvc: MapMaplibreService;
+  const mapSize = [1024, 768];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapMaplibreComponent ]
-    })
-    .compileComponents();
+      declarations: [MapMaplibreComponent],
+      providers: [
+        MapMaplibreService,
+        { provide: LayersService, useClass: LayersService },
+        { provide: MapStateService, useClass: MapStateService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MapMaplibreComponent);
     component = fixture.componentInstance;
+    component.layersSvc = new LayersService();
+    component.mapStateSvc = new MapStateService();
+    mapSvc = TestBed.inject(MapMaplibreService);
     fixture.detectChanges();
+    component.map._container.style.height = `${mapSize[1]}px`;
+    component.map._container.style.width = `${mapSize[0]}px`;
+    component.map.resize();
+    fixture.detectChanges();
+
+    // https://github.com/maplibre/maplibre-gl-js/discussions/2193
+    await new Promise((resolve, reject) => {
+      // idle
+      component.map.once('load', (evt) => {
+        resolve(evt);
+      });
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a map and the map should be the same as from the mapSvc', () => {
+    expect(component.map instanceof Map).toBeTruthy();
+    expect(mapSvc.map.getValue()._mapId).toBe(component.map._mapId);
+  });
+
+  it('should add/update Layers to the style', () => {
+    const { layers } = addSomeLayers(component, mapSvc);
+    const mapStyle = component.map.getStyle();
+    expect(mapStyle.layers.length).toBe(layers.length);
+    expect(mapStyle.metadata[`ukis:LayersIDs`]).toEqual(layers.map(l => l.id));
   });
 });
