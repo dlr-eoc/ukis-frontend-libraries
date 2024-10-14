9ced053dac2cfb35e678c5fe1622b3ba0db80a9a
Mathias Boeck <Mathias.Boeck@dlr.de>
2024-10-14 09:00:39 +0200

    BREAKING CHANGE: refactor map-maplibre to standalone

--- projects/map-maplibre/src/public-api.ts
+++ projects/map-maplibre/src/public-api.ts
@@ -6,4 +6,3 @@ export * from './lib/maplibre.helpers';
 export * from './lib/maplibre-layers.helpers';
 export * from './lib/map-maplibre.service';
 export * from './lib/map-maplibre.component';
-export * from './lib/map-maplibre.module';
