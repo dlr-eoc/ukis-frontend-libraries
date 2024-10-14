ac48c2dd83f12df07cd45b116e3360b64920df2e
Mathias Boeck <Mathias.Boeck@dlr.de>
2024-10-14 09:07:06 +0200

    BREAKING CHANGE: refactor services-map-state to standalone

--- projects/services-map-state/src/lib/map-state.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MapStateService } from './map-state.service';
-@NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
-  providers: [MapStateService]
-})
-export class MapStateModule { }
