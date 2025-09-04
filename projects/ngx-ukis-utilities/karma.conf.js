6d87c82d0276ed41cae0aaceccef3248a6220933
Mathias Boeck <Mathias.Boeck@dlr.de>
2025-09-04 11:20:06 +0200

    fix (ngx-ukis-ui-clarity): path in karma conf

--- projects/ngx-ukis-utilities/karma.conf.js
+++ projects/ngx-ukis-utilities/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/utilities'),
+      dir: require('path').join(__dirname, '../../coverage/ngx-ukis-ui-clarity'),
       subdir: '.',
       reporters: [
         { type: 'html' },
