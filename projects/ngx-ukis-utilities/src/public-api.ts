b2f47e51413fc8531400172974d2476378dad5f3
Mathias Boeck <Mathias.Boeck@dlr.de>
2025-09-09 10:51:35 +0200

    refactor (ngx-ukis-utilities): adjust docs and remove unused files from the library generation

--- projects/ngx-ukis-utilities/src/public-api.ts
+++ projects/ngx-ukis-utilities/src/public-api.ts
@@ -1,7 +1,5 @@
 /*
  * Public API Surface of ngx-ukis-utilities
  */
-
-export * from './lib/ngx-ukis-utilities';
 export * from './lib/pipes';
 export * from './lib/dynamic-component/dynamic-component';
