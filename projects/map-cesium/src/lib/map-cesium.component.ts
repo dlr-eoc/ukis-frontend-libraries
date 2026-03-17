a5d0f30918af7e3a3f54b46fcc26f1c294604c34
ange_lu <lucas.angermann@dlr.de>
2026-03-17 08:53:00 +0100

    fix: adjust cesium destroy function

--- projects/map-cesium/src/lib/map-cesium.component.ts
+++ projects/map-cesium/src/lib/map-cesium.component.ts
@@ -104,7 +104,7 @@ export class MapCesiumComponent implements OnInit, AfterViewInit, OnDestroy {
       this.viewer.imageryLayers.removeAll();
       this.viewer.dataSources.removeAll();
       this.viewer.scene.primitives.removeAll();
-      //this.viewer.scene.primitives.destroy(); -> will lead to developer errors due to missing elements
+      this.viewer.destroy();
     }
     this.mapSvc.destroyLayerGrpoups();
   }
